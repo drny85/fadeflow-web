3a5def284e1b886cbd6938bf87b77f17cd8c1709
Robert Melendez <robertm3lendez@gmail.com>
2024-09-27 20:20:51 -0400

    change thme color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DownloadLinks from '@/components/DownloadLinks';
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen min-w-full flex-col items-center justify-between bg-[#773344]">
+      <main className="flex min-h-screen min-w-full flex-col items-center justify-between bg-[#1D3557]">
          <img src="/images/banner.png" alt="banner" className="w-full h-full object-fill overflow-hidden" />
          <DownloadLinks />
       </main>
